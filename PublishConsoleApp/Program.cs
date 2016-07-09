6fc1530dbc4e18e2fd6b24cf7f95715af00bf03f
Carlos Gómez <carlos.carrero@veinteractive.com>
2016-07-09 17:47:11 +0200

    Add a Installer Project

--- PublishConsoleApp/Program.cs
+++ PublishConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PublishConsoleApp
 {
@@ -10,6 +7,19 @@ namespace PublishConsoleApp
     {
         static void Main(string[] args)
         {
+            string json = @"{
+                'Email': 'james@example.com',
+                'Active': true,
+                'CreatedDate': '2013-01-20T00:00:00Z',
+                'Roles': [
+                    'User',
+                    'Admin'
+                ]
+            }";
+
+            Account account = JsonConvert.DeserializeObject<Account>(json);
+            Console.WriteLine(account.Email);
+            Console.ReadLine();
         }
     }
 }
